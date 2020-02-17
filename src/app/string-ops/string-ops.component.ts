9012002e92e2a8f6ecbda642a37ec24403f55b27
Irinyi Kabinet Felhasználó <valaki@pcv6312-9.inf.u-szeged.hu>
2020-02-17 18:47:12 +0100

    ora vegi allapot

--- src/app/string-ops/string-ops.component.ts
+++ src/app/string-ops/string-ops.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StringOpsComponent implements OnInit {
 
+  str1: string;
+  str2: string;
+  result: string
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  concat() {
+    this.result = this.str1 + ' ' + this.str2;
   }
 
 }
